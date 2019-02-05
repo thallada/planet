1743145944d4de565f139e7827bcfdc29aa25c5d
Tyler Hallada <tyler@hallada.net>
2019-02-04 23:47:45 -0500

    Detail level 8 crashed my computer. How about 6...

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 console.time("init geo");
-var geometry = new THREE.IcosahedronGeometry(10, 8);
+var geometry = new THREE.IcosahedronGeometry(10, 6);
 console.timeEnd("init geo");
 
 console.time("Hexsphere");
